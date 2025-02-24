fb510778f3bc4fb28978b84d1f5562f777774a76
CHARLIE <deluxerelax4@gmail.com>
2025-02-24 11:49:10 +0800

    UD

---
+++ Lapuz_IT13B_activities/src/lapuz_it13b_activities/Lapuz_IT13B_activities.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lapuz_it13b_activities;
+
+/**
+ *
+ * @author User
+ */
+public class Lapuz_IT13B_activities {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        public class Lab_act1_Arithmetics {
+    
+        
+      int first1= 10;
+      int second2 = 6;
+      int third3 = 4;
+      
+      
+      int addition = first1 + second2 + third3;
+      int subraction = first1 - second2 - third3;
+      int multiply = first1 * second2 * third3 ;
+      int divide = first1 / second2 / third3;
+      int module = first1 % second2 % third3;
+      
+      
+      System.out.println("  10 * 4 + 6  =" + addition);
+      System.out.println (" (  10-4) %  6 =" + subraction);
+      System.out.println ("( 10 / 4 + 6)/3 ="+  multiply);
+      System.out.println(" 10 *  6  -( 4 *  4)="  +  divide);
+    }
+    
+}
