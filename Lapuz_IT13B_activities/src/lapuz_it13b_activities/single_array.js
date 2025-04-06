669013eef61b2a3390290cd90d0081feefcc65f6
KristiaLorraine <20240089@nbsc.edu.ph>
2025-04-06 20:33:36 +0800

    added single array

---
+++ Lapuz_IT13B_activities/src/lapuz_it13b_activities/single_array.js
@@ -0,0 +1,16 @@
+package midterm;
+
+
+public class lapuz_single_array {
+        public static void main(String[] args) {
+        
+        // SINGLE ARRAY
+        char letters[]= {'A','B','C','D','E','F','G'}; 
+        
+        for (char l = 0; l < letters.length; l++) {
+            System.out.print(letters[l] + " , ");
+          
+        }
+        // 
+    }
+}
